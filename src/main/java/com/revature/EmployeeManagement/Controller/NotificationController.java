789c30c3c83db89388b1519442ab3fcf4d93d431
Mahlet D <mahletd@Mahlets-MacBook-Pro.local>
2023-04-18 14:46:34 -0400

    notification modified

--- src/main/java/com/revature/EmployeeManagement/Controller/NotificationController.java
+++ src/main/java/com/revature/EmployeeManagement/Controller/NotificationController.java
@@ -1,11 +1,14 @@
 package com.revature.EmployeeManagement.Controller;
 
+import com.revature.EmployeeManagement.Exception.InvalidCredential;
 import com.revature.EmployeeManagement.Model.Employee;
 import com.revature.EmployeeManagement.Model.Leave;
 import com.revature.EmployeeManagement.Model.Notification;
 import com.revature.EmployeeManagement.Service.LeaveService;
 import com.revature.EmployeeManagement.Service.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -27,4 +30,20 @@ public class NotificationController {
         return notificationService.getAllNotifications();
     }
 
+
+
+    @GetMapping("/manager/{managerId}")
+    public List<Notification> getNotificationByManager(@PathVariable Long managerId){
+        return notificationService.getNotificationByManager(managerId);
+    }
+
+    @GetMapping("/employee/{employeeId}")
+    public List<Notification> getNotificationByEmployee(@PathVariable Long employeeId){
+        return notificationService.getNotificationByEmployee(employeeId);
+    }
+
+    @ExceptionHandler(InvalidCredential.class)
+    public ResponseEntity<String > handleResourceNotFoundExceptions(InvalidCredential e){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("The requested resource not Found");
+    }
 }
--- src/main/java/com/revature/EmployeeManagement/Repositoty/NotificationRepository.java
+++ src/main/java/com/revature/EmployeeManagement/Repositoty/NotificationRepository.java
@@ -10,4 +10,7 @@ import java.util.List;
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
     List<Notification> findByEmployee(Employee employee);
     List<Notification> findByEmployee_ManagerId(Long managerId);
+    List<Notification> findByManagerId(Long employeeId);
+
+    List<Notification> findByEmployeeId(Long employeeId);
 }
