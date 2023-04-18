dfb100258fd546928e84c8efebd868c1912745f3
Mahlet D <mahletd@Mahlets-MacBook-Pro.local>
2023-04-18 15:32:49 -0400

    assigning goal to an employee added

--- src/main/java/com/revature/EmployeeManagement/Repositoty/GoalRepository.java
+++ src/main/java/com/revature/EmployeeManagement/Repositoty/GoalRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 @Repository
 public interface GoalRepository extends JpaRepository<Goal, Long> {
@@ -12,4 +13,6 @@ public interface GoalRepository extends JpaRepository<Goal, Long> {
 
 
     Optional<Goal> findByNameAndDeadlineAndEmployeeId(String name, LocalDate deadline, long employeeId);
+
+    List<Goal> findByEmployeeId(long employeeId);
 }
