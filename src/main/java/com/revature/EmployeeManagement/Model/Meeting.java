bdc0df9abb1b64ca265a74a1e2eabeadce66d27a
Mahlet D <mahletd@Mahlets-MacBook-Pro.local>
2023-04-20 20:17:03 -0400

    meeting entity added

---
+++ src/main/java/com/revature/EmployeeManagement/Model/Meeting.java
@@ -0,0 +1,38 @@
+package com.revature.EmployeeManagement.Model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+public class Meeting{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String subject ;
+    @Column
+    private LocalDate startTime;
+    @Column
+    private LocalDate endTime;
+    @Column
+    private String description;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JsonBackReference
+    @JoinColumn(name = "employeeId")
+    private Employee employee;
+
+    @Column(name = "employeeId", insertable = false, updatable = false)
+    private long employeeId;
+
+}
