3b142efcb9c3b22fb7f93cc13a7146c1319c9183
Mahlet D <mahletd@Mahlets-MacBook-Pro.local>
2023-04-23 20:46:37 -0400

    holiday modified

--- src/main/java/com/revature/EmployeeManagement/Controller/HolidayController.java
+++ src/main/java/com/revature/EmployeeManagement/Controller/HolidayController.java
@@ -4,18 +4,18 @@ import com.revature.EmployeeManagement.Exception.InvalidCredential;
 import com.revature.EmployeeManagement.Model.Holiday;
 import com.revature.EmployeeManagement.Repositoty.HolidayRepository;
 import com.revature.EmployeeManagement.Service.HolidayService;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @CrossOrigin("*")
+@NoArgsConstructor
 public class HolidayController {
 
     HolidayService holidayService;
@@ -25,15 +25,48 @@ public class HolidayController {
     }
 
 
+    /**
+     * post holidays
+     * ENDPOINT POST localhost:9000/holidays
+     * @param holiday
+     * @return
+     */
+    @PostMapping("/holidays")
+    public ResponseEntity<String> postHolidays(@RequestBody Holiday holiday){
+        try {
+            holidayService.addHolidays(holiday);
+            return ResponseEntity.ok().body("Holiday added successfully");
+        }
+        catch (InvalidCredential e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
 
+    /**
+     * Get all holidays
+     * ENDPOINT GET localhost:9000/holidays
+     * @return
+     */
     @GetMapping("/holidays")
     public List<Holiday> getAllHolidays(){
         return holidayService.getAllHoliday();
     }
+
+    /**
+     * Get future holidays
+     * ENDPOINT localhost:9000/holidays/future
+     * @return
+     */
+    @GetMapping("/holidays/future")
+    public List<Holiday> getFutureHolidays(){
+        return holidayService.getFutureHolidays();
+    }
+
 
 
     @ExceptionHandler(InvalidCredential.class)
     public ResponseEntity<String > handleResourceNotFoundExceptions(InvalidCredential e){
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body("The requested resource not Found");
     }
+
 }
